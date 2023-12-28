4fd5989e71e27ef529f7fab62602d84e31caeca6
krishna vineeth <gvsskvineeth@gmail.com>
2023-12-29 01:46:08 +0530

    first

---
+++ config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBKewrCsLb7rz-P-QfKzJw7a93Cok3S5VA",
+    authDomain: "ibm-coderush.firebaseapp.com",
+    databaseURL: "https://ibm-coderush-default-rtdb.firebaseio.com",
+    projectId: "ibm-coderush",
+    storageBucket: "ibm-coderush.appspot.com",
+    messagingSenderId: "521680123643",
+    appId: "1:521680123643:web:31cd49ea3246863889225e",
+    measurementId: "G-NYYEL2EZ9Q"
+};
+
+
+export default firebaseConfig;
---
+++ sofa.js
@@ -0,0 +1,70 @@
+let sofa = [
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "./sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    
+    
+];
