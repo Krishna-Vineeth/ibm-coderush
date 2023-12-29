4592c39b2642fee53e6a5f8d95847b3a79cffa0c
krishna vineeth <gvsskvineeth@gmail.com>
2023-12-29 11:40:39 +0530

    final evaluation

---
+++ static/js/testing.js
@@ -0,0 +1,167 @@
+let sofa = [
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    
+    
+    
+];
---
+++ static/js/text.js
@@ -0,0 +1,224 @@
+let sofa = [
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+    {
+        name: "Loveseat sofa",
+        image: "/static/sofa/img@2x.png",
+        price: "Rs. 25,500.00",
+        strikeprice: "Rs. 30,000.00",
+    },
+
+    {
+        name: "Comfortable Velvet Sofa",
+        image: "/static/sofa/velvet_sofa_1.png",
+        price: "Rs. 38,999.00",
+        strikeprice: "Rs. 45,000.00",
+    },
+    {
+        name: "Luxury Leather Recliner",
+        image: "/static/sofa/leather_recliner_1.png",
+        price: "Rs. 55,500.00",
+        strikeprice: "Rs. 65,000.00",
+    },
+    {
+        name: "Minimalist Linen Sofa",
+        image: "/static/sofa/linen_sofa_1.png",
+        price: "Rs. 32,750.00",
+        strikeprice: "Rs. 40,000.00",
+    },
+    {
+        name: "Cozy Sectional Couch",
+        image: "/static/sofa/sectional_couch_1.png",
+        price: "Rs. 48,999.00",
+        strikeprice: "Rs. 55,000.00",
+    },
+    {
+        name: "Elegant Fabric Sofa",
+        image: "/static/sofa/fabric_sofa_1.png",
+        price: "Rs. 42,500.00",
+        strikeprice: "Rs. 50,000.00",
+    },
+    {
+        name: "Modern Modular Sofa",
+        image: "/static/sofa/modular_sofa_1.png",
+        price: "Rs. 51,999.00",
+        strikeprice: "Rs. 60,000.00",
+    },
+    {
+        name: "Vintage Chesterfield Sofa",
+        image: "/static/sofa/chesterfield_sofa_1.png",
+        price: "Rs. 49,500.00",
+        strikeprice: "Rs. 58,000.00",
+    },
+    {
+        name: "Sleek Futon Sofa Bed",
+        image: "/static/sofa/futon_sofa_bed_1.png",
+        price: "Rs. 28,999.00",
+        strikeprice: "Rs. 35,000.00",
+    },
+    {
+        name: "Rustic Leather Loveseat",
+        image: "/static/sofa/leather_loveseat_1.png",
+        price: "Rs. 37,500.00",
+        strikeprice: "Rs. 45,000.00",
+    },
+    {
+        name: "Chic Velvet Sleeper Sofa",
+        image: "/static/sofa/velvet_sleeper_sofa_1.png",
+        price: "Rs. 44,999.00",
+        strikeprice: "Rs. 50,000.00",
+    },
+    {
+        name: "Traditional Plaid Sofa",
+        image: "/static/sofa/plaid_sofa_1.png",
+        price: "Rs. 33,750.00",
+        strikeprice: "Rs. 38,000.00",
+    },
+    {
+        name: "Compact Loveseat",
+        image: "/static/sofa/compact_loveseat_1.png",
+        price: "Rs. 26,500.00",
+        strikeprice: "Rs. 32,000.00",
+    },
+    {
+        name: "Futuristic Adjustable Sofa",
+        image: "/static/sofa/adjustable_sofa_1.png",
+        price: "Rs. 39,999.00",
+        strikeprice: "Rs. 48,000.00",
+    },
+    {
+        name: "Plush Microfiber Sofa",
+        image: "/static/sofa/microfiber_sofa_1.png",
+        price: "Rs. 34,500.00",
+        strikeprice: "Rs. 40,000.00",
+    },
+    {
+        name: "Sculptural Modern Sofa",
+        image: "/static/sofa/sculptural_sofa_1.png",
+        price: "Rs. 53,999.00",
+        strikeprice: "Rs. 62,000.00",
+    },
+    {
+        name: "Geometric Patterned Sofa",
+        image: "/static/sofa/patterned_sofa_1.png",
+        price: "Rs. 31,750.00",
+        strikeprice: "Rs. 38,000.00",
+    },
+    {
+        name: "Lively Color Block Sofa",
+        image: "/static/sofa/color_block_sofa_1.png",
+        price: "Rs. 46,500.00",
+        strikeprice: "Rs. 55,000.00",
+    },
+    {
+        name: "Transitional Sleeper Sofa",
+        image: "/static/sofa/transitional_sleeper_sofa_1.png",
+        price: "Rs. 42,999.00",
+        strikeprice: "Rs. 50,000.00",
+    },
+    {
+        name: "Industrial Style Sofa",
+        image: "/static/sofa/industrial_sofa_1.png",
+        price: "Rs. 36,750.00",
+        strikeprice: "Rs. 45,000.00",
+    },
+
+    {
+        name: "Velvet Luxury Sofa",
+        image: "/static/sofa/velvet_luxury_sofa.png",
+        price: "Rs. 42,999.00",
+        strikeprice: "Rs. 50,000.00",
+    },
+    {
+        name: "L-Shaped Sectional",
+        image: "/static/sofa/l_shaped_sectional.png",
+        price: "Rs. 38,500.00",
+        strikeprice: "Rs. 45,000.00",
+    },
+
+    {
+        name: "Convertible Sleeper Sofa",
+        image: "/static/sofa/convertible_sleeper_sofa.png",
+        price: "Rs. 29,999.00",
+        strikeprice: "Rs. 35,000.00",
+    },
+    
+    {
+        name: "Classic Leather Couch",
+        image: "/static/sofa/classic_leather_couch.png",
+        price: "Rs. 48,750.00",
+        strikeprice: "Rs. 55,000.00",
+    },
+
+    {
+        name: "Coastal Style Sofa Set",
+        image: "/static/sofa/coastal_style_sofa_set.png",
+        price: "Rs. 49,999.00",
+        strikeprice: "Rs. 58,000.00",
+    },
+    {
+        name: "Urban Chic Corner Sofa",
+        image: "/static/sofa/urban_chic_corner_sofa.png",
+        price: "Rs. 44,500.00",
+        strikeprice: "Rs. 52,000.00",
+    },
+    {
+        name: "Plush Sectional with Ottoman",
+        image: "/static/sofa/plush_sectional_ottoman.png",
+        price: "Rs. 55,999.00",
+        strikeprice: "Rs. 65,000.00",
+    },
+    {
+        name: "Minimalist Gray Sofa",
+        image: "/static/sofa/minimalist_gray_sofa.png",
+        price: "Rs. 31,500.00",
+        strikeprice: "Rs. 38,000.00",
+    },
+    {
+        name: "Retro Fabric Sofa",
+        image: "/static/sofa/retro_fabric_sofa.png",
+        price: "Rs. 36,999.00",
+        strikeprice: "Rs. 45,000.00",
+    },
+    {
+        name: "Midnight Blue Loveseat",
+        image: "/static/sofa/midnight_blue_loveseat.png",
+        price: "Rs. 33,500.00",
+        strikeprice: "Rs. 40,000.00",
+    },
+    {
+        name: "Classic Leather Couch",
+        image: "/static/sofa/classic_leather_couch.png",
+        price: "Rs. 48,750.00",
+        strikeprice: "Rs. 55,000.00",
+    },
+    {
+        name: "Convertible Sleeper Sofa",
+        image: "/static/sofa/convertible_sleeper_sofa.png",
+        price: "Rs. 29,999.00",
+        strikeprice: "Rs. 35,000.00",
+    },
+
+];
